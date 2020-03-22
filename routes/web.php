e4e1d570edec99258286d7da902613c4a26e488f
Promsurin Phutthammawong <promsurinm@hotmail.com>
2020-03-23 01:54:43 +0700

    API Change

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/api','HomeController@index')->name('def')->with(Session::flash('error','Method not allow'));
 Route::post('/api','APIController@data_incoming')->name('api.getdata');
+
 Route::prefix('admin')->group(function ()
 {
     Route::get('/user', 'User\AdminController@index')->name('user.management');
